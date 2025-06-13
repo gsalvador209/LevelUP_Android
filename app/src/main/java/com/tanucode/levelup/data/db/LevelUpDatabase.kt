ba90ab0f1886da7afe84e85c16983b0bbec26c73
Chava <dchavezvillanueva@gmail.com>
2025-06-13 16:32:32 -0600

    build: database migration for new column (repetition)

--- app/src/main/java/com/tanucode/levelup/data/db/LevelUpDatabase.kt
+++ app/src/main/java/com/tanucode/levelup/data/db/LevelUpDatabase.kt
@@ -21,7 +21,7 @@ import com.tanucode.levelup.util.Constants
             ListEntity::class,
             UserEntity::class
                ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 
